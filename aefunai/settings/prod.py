f7c3d8b129857c8bdb648b2f7637d82786aae5f1
Gentility01 <mastergentility5@gmail.com>
2023-10-29 08:40:19 +0100

    changed database

--- aefunai/settings/prod.py
+++ aefunai/settings/prod.py
@@ -11,13 +11,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'aefunai-feedbacks.onrender.com']
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}

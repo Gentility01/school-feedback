c3c9494a679ebb9a12f65b200d8d0f79e5d3ce28
Gentility01 <mastergentility5@gmail.com>
2023-10-29 09:00:02 +0100

    uodated models

--- users/models.py
+++ users/models.py
@@ -17,7 +17,7 @@ class Student(models.Model):
     user = models.OneToOneField(MainUser, on_delete=models.CASCADE)
     gender = models.CharField(max_length=20,choices=Gender.choices,  default=Gender.Male,)
     faculty = models.CharField(max_length=50, choices=Faculty.choices, default=Faculty.agric)
-    registration_number = models.CharField(max_length=10, unique=True)
+    registration_number = models.CharField(max_length=10, unique=True, blank=True, null=True)
     # Add other student-related fields here
 
 
@@ -29,7 +29,7 @@ class Student(models.Model):
 class Lecturer(models.Model):
     user = models.OneToOneField(MainUser, on_delete=models.CASCADE)
     department = models.CharField(max_length=50, choices=Departments.choices, default=Departments.ART)
-    lecturer_id = models.CharField(max_length=10, unique=True)
+    lecturer_id = models.CharField(max_length=10, unique=True, blank=True, null=True)
     position = models.CharField(max_length=50, choices=Administrator.choices, default=Administrator.dean)
     # Add other lecturer-related fields here
 
